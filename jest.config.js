9ba28df00a7ed0179de8bfcd2989fd3a6e57e327
Abraham Williams <4braham@gmail.com>
2020-08-07 19:12:14 -0500

    Run linters in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,27 @@
 module.exports = {
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(tsx?)$',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+  projects: [
+    {
+      displayName: 'Tests',
+      preset: 'ts-jest',
+      testMatch: ['<rootDir>/src/**/*.test.ts'],
+      transform: {
+        '^.+\\.[t|j]sx?$': 'ts-jest',
+      },
+    },
+    {
+      displayName: 'ESLint',
+      runner: 'eslint',
+      testMatch: ['<rootDir>/**/*.ts'],
+    },
+    {
+      displayName: 'Prettier',
+      preset: 'jest-runner-prettier',
+      testPathIgnorePatterns: ['/node_modules/', '/out-tsc/', '/dist/', '/package-lock.json/', '^.+\\.js$'],
+    },
+    {
+      displayName: 'TypeScript',
+      runner: 'tsc',
+      testPathIgnorePatterns: ['^.+\\.js$'],
+    },
+  ],
 };
